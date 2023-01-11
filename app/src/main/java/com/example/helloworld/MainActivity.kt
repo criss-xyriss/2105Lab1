51e6b35f11a8e7e2bfab5e9120ee0d386dc12e08
Kage-hime <changweesiangwilson@gmail.com>
2023-01-11 15:12:02 +0800

    Testing

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("LOL", "TestLog")
+        Log.v("LOL", "TestLog")
 
     }
 
